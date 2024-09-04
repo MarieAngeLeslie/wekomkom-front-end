f758c79730526301ce054a34ee7cbce90333f604
MarieAngeLeslie <42603506+MarieAngeLeslie@users.noreply.github.com>
2024-09-04 07:08:34 +0000

    navbar done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import styles from "../styles/nav.module.css";
-
+import Image from 'next/image'
+import Link from 'next/link'
+import { MsgIcon, BellIcon, BottomChevronIcon } from '../images/svg/svgIcon'
 
 export const metadata: Metadata = {
   title: "wekomkom",
@@ -16,7 +18,45 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={styles.body}>
-        <nav className={styles.nav} >General nav here</nav>
+        <nav className={styles.nav} >
+          <div className={styles["first-side"]}>
+            <Image
+              src={"/logo.png"}
+              alt="Picture of the author"
+              width={168}
+              height={70}
+            />
+            <ul>
+              <li>
+                <Link href="/">Accueil</Link>
+              </li>
+              <li>
+                <Link href="/opportunities">Opportunités</Link>
+              </li>
+              <li>
+                <Link href="/boutique">Boutique</Link>
+              </li>
+            </ul>
+          </div>
+          <div className={styles["second-side"]}>
+            <MsgIcon />
+            <BellIcon />
+            <div className={styles["profil-bloc"]} >
+              <Image
+                src={'/happy-woman-profil.jpg'}
+                alt="Picture of the author"
+                sizes="300px"
+                fill
+                style={{
+                  objectFit: 'cover',
+                }}
+              />
+              <div className={styles["bottom-chevron"]}>
+                <BottomChevronIcon />
+              </div>
+            </div>
+          </div>
+        </nav>
         {children}
       </body>
     </html>
---
+++ src/app/opportunities/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './opportunities.module.css'
+
+export default function Opportunites() {
+    return (
+        <main className={styles.main}>
+            <main className={styles.header}>
+
+            </main>
+        </main>
+    )
+}
